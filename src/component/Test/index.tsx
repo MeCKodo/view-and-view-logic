af615bcdf929d7a592ecb7ffbf1c214291a68ae6
kodo <524580860@qq.com>
2019-07-27 15:29:33 +0800

    withViewmodel decorator

--- src/component/Test/index.tsx
+++ src/component/Test/index.tsx
@@ -1,37 +1,24 @@
 import React from 'react';
-import { observer } from 'mobx-react-lite';
+import { observer } from 'mobx-react';
 import { withViewModel } from '../../hoc';
 import { TestVM } from './TestVM';
 import { Props } from './types';
 
-const TestComp1 = React.memo(
-  observer((props: Props) => {
-    const { vm } = props;
-    console.log(props, '--props');
+@observer
+@withViewModel<Props>(TestVM)
+class TestComp extends React.Component<Props> {
+  render() {
+    const { vm } = this.props;
+
     return (
       <div onClick={vm.setUserName}>
-        {vm.userName}
-        <p>name111 ={vm.name111}</p>
-        <p>name222 ={vm.name222}</p>
+        {vm.userName} name ={vm.name111}
       </div>
     );
-  }),
-);
-
-// @observer
-// class TestComp extends React.Component<Props> {
-//   render() {
-//     const { vm } = this.props;
-
-//     return (
-//       <div onClick={vm.setUserName}>
-//         {vm.userName} name ={vm.name111}
-//       </div>
-//     );
-//   }
-// }
+  }
+}
 
 // const Test = withViewModel<Props>(TestComp, TestStore);
-const TestC = withViewModel<Props>(TestComp1, TestVM);
+// const TestC = withViewModel<Props>(TestVM);
 
-export { TestC };
+export { TestComp };
